9fd5045d58772c6950c052ca4c31ae98f8b5b5f3
Owen Comstock <owen.comstock@gmail.com>
2022-11-11 14:42:52 -0500

    Blanked donut slices, one side 12" record

--- donutmaker.R
+++ donutmaker.R
@@ -10,17 +10,40 @@ library(googlesheets4)
 tracks <- read_csv('donutstracks.csv',
                    show_col_types = FALSE)
 
-donutbox <- ggplot(data = tracks, 
-                   aes(x = reorder(trackname, donutstrack, 
-                                   order = is.ordered(trackname), decreasing = FALSE), 
-                       y = length)) + 
-  geom_bar(stat = "identity") +
-  scale_y_discrete(name = "test") +
-  coord_flip() +
-  
-  theme_minimal() +
-  theme(axis.title.y = element_blank()) 
 
-print(donutbox)                   
 
-ggsave(file="donutbox.svg", plot=donutbox, width=2, height=2)
+# Set up the whole album donut chart
+# Compute percentages and cumulative percentages
+tracks$fraction = tracks$length / sum(tracks$length)
+# ymax is the right (clockwise) edge of each slice
+tracks$ymax = cumsum(tracks$fraction)
+# ymin is the left (clockwise) edge of each slice
+tracks$ymin = c(0, head(tracks$ymax, n=-1))
+
+# Make the plot. 
+donutrecord <- ggplot(tracks, aes(ymax=ymax, ymin=ymin, 
+                               xmax=5.75, xmin=1.5)) + #outermost edge and center hole radius
+  geom_rect(fill = NA, color = 'gray80') + # rectangles that will be converted to 4-edged slices
+  coord_polar(theta='y') + # converts rectangles to radial slices
+  xlim(c(0, 6)) + # radius of a 12" record
+  theme_void() +
+  theme(legend.position = 'none')
+
+print(donutrecord)                   
+
+# Save the plot as a 12" SVG
+ggsave(file="donutrecord.svg", plot=donutrecord, width=9, height=9)
+
+# donutbox <- ggplot(data = tracks, 
+#                    aes(x = reorder(trackname, donutstrack, 
+#                                    order = is.ordered(trackname), decreasing = FALSE), 
+#                        y = length)) + 
+#   geom_bar(stat = "identity") +
+#   scale_y_continuous(name = "track length in seconds") +
+#   coord_flip() +
+#   theme_minimal() +
+#   theme(axis.title.y = element_blank()) 
+# 
+# print(donutbox)                   
+# 
+# ggsave(file="donutbox.svg", plot=donutbox, width=7, height=7)
