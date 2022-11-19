24abb275012906f6576b3d2e4c6459e957df80ca
comstockery <owen.comstock@gmail.com>
2022-11-18 20:26:15 -0500

    Add samples by year svg

--- donutmaker.R
+++ donutmaker.R
@@ -6,6 +6,13 @@ library(ggtext)
 library(googlesheets4)
 library(emojifont)
 
+# Flags for code chunks
+datamaker = TRUE; # TRUE at the start of a session; otherwise FALSE
+donutmaker = FALSE; # TRUE to generate main album donut; otherwise FALSE
+
+
+if(datamaker) {
+
 # Read data into R from Google Sheets
 donutsmain <- read_sheet('https://docs.google.com/spreadsheets/d/1hv66GrSazsAs6QvlvOZpnHyJc4u6_tbLDQ5m3wMPwWw/edit#gid=1315766256') %>%
   select(1:15) # Drop the last three rows
@@ -27,6 +34,7 @@ instancelist <- donutsmain %>%
   select(donutstrack, trackname, sampledtrack:duration) %>%
   select(-c(sampledyear, sampledelement))
 
+} # End of the datamaker code chunk
 
 # Start making donuts!
 
@@ -50,8 +58,6 @@ tracklist$ymin = c(0, head(tracklist$ymax, n=-1))
 # Set up each sprinkle (dots)
 # Time goes from the outer edge (confusingly: lead in) to inner edge (lead out)
 # Scale time from 1955 to 2005 for ease of axes 
-# latestyear = max(samplelist$sampledyear)
-# earliestyear = min(samplelist$sampledyear)
 latestyear = 2005
 earliestyear = 1955
 samplelist$radius = 1 - ((samplelist$sampledyear-earliestyear) / (latestyear-earliestyear))
@@ -64,13 +70,11 @@ vlinecalc <- function(year) {
   return(vlinegroove)
 }
 
-# samplelist$groove = vlinecalc(samplelist$sampleyear)
-
 # Join datasets to map samples to the Donut tracklist
-df = inner_join(tracklist, samplelist, by='donutstrack')
+df1 = inner_join(tracklist, samplelist, by='donutstrack')
 
 # Place sprinkles within their slice, adding randomness to avoid overplotting
-df$sprinkle = df$ymin + ((df$ymax - df$ymin)*(runif(nrow(df), 0.10, 0.90)))
+df1$sprinkle = df1$ymin + ((df1$ymax - df1$ymin)*(runif(nrow(df1), 0.10, 0.90)))
 
 
 # Make a table for year labels
@@ -80,12 +84,14 @@ yearlabels <- data.frame(
   y = 1,
   angle = 0)
 
+# Count the genres in dataset
+genres = count(df1, sampledgenre)
 
-# Check that there are only 5 genres in dataset
-genres = count(df, sampledgenre)
 
-# Make the donut
-donutrecord <- ggplot(df) + 
+# Make the donut for entire album, but only if the donutmaker is TRUE!
+if(donutmaker) {
+
+donutrecord <- ggplot(df1) + 
   # Create two sets of rectangles that will be converted to 4-edged slices
   # First, the rectangles for the track slices
   geom_rect(aes(ymax = ymax, ymin = ymin, 
@@ -96,24 +102,20 @@ donutrecord <- ggplot(df) +
   geom_rect(aes(ymax = 0, ymin = 1,
                 xmax = outer, xmin = inner),
             fill = NA, color = 'gray33', 
-            linewidth = 0.5) +
+            linewidth = 0.6) +
   
   # Add gridlines for certain years (grooves)
   geom_vline(xintercept = vlinecalc(c(1970,1990)), color = 'gray77') +
-  # geom_rect(aes(ymax = ymax, ymin = ymin,
-  #               xmax = groove,
-  #               xmin = groove-sprinklewidth, 
-  #               fill = sampledgenre), color = 'gray77') + #widesprinkles
-  
+
   # Add labels for gridlines (grooves)
   geom_richtext(data = yearlabels,
                 mapping = aes(x = x, y = y, label = label,
                               hjust = 0.5, vjust = 0.5,
                               angle = angle),
-                color = 'gray50', fill = 'white', 
+                color = 'gray50', fill = NA, 
                 label.color = NA,
-                size = 6,
-                label.padding = grid::unit(rep(0, 4), "pt")) +
+                size = 4,
+                label.padding = unit(rep(0, 4), "pt")) +
  
   # Add dots representing samples in each track (sprinkles)
   geom_point(aes(x = groove, y = sprinkle,
@@ -126,20 +128,59 @@ donutrecord <- ggplot(df) +
   # geom_text(aes(x = groove, y = sprinkle, 
   #               label = emoji('cow')), family="OpenSansEmoji", size=5) + #emoji?
   
-  # Converts rectangles to radial slices
-  coord_polar(theta = 'y') + 
-  
   # Remove any gaps around the plot edge 
   scale_x_continuous(limits = c(0, 6), expand = expansion(0,0)) +
-  scale_y_continuous(limits = c(0, 1), expand = expansion(0,0)) + 
+  scale_y_continuous(limits = c(0, 1), expand = expansion(0,0),
+                     labels = NULL) +
   
+# Converts rectangles to radial slices
+  coord_polar(theta = 'y') +
   # Remove other plot elements and legend
   theme_void() +
-  theme(legend.position = 'none')
+  theme(legend.position = 'none', 
+        panel.border = element_blank(),
+        legend.key = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.y = element_blank(),
+        panel.grid  = element_blank(),
+        plot.margin = unit(rep(-0.5,4), "inches"))
 
-donutrecord
-
+print(donutrecord)
 
 # Save the plot as an SVG
 ggsave(file = "donutrecord.svg", plot = donutrecord, 
        width = 7, height = 7, dpi = 72)
+
+} # End of the 'donutmaker' code chunk
+
+
+# Make a timeline of the sampled songs' release years (count, stacked by genre)
+df2 <- samplelist %>%
+  distinct(sampledtrack, .keep_all = TRUE) 
+
+# Count the number of samples of each genre for each year. 
+# Plot the values on an axis from 1955 through 2005. 
+samplesbyyear <- ggplot(df2,
+       aes(sampledyear, fill = sampledgenre)) +
+  geom_bar(color = "gray77", width = 1) +
+  scale_fill_manual(values = c("#E67343", "#F72585", "#F8B219", "#004AF7", "#9B53E6")) +
+# Remove any gaps around the plot edge 
+  scale_x_continuous(limits = c(1955, 2005), expand = expansion(0,0)) +
+  scale_y_continuous(limits = c(0, 8), expand = expansion(add = c(0, 1)),
+                     breaks = seq(0, 8, 2)) +
+  theme_minimal() +
+  theme(legend.position = 'none',
+        axis.line.x = element_line(color = "gray55"),
+        axis.ticks = element_blank(),
+        axis.ticks.x = element_line(color = "gray77"),
+        axis.title = element_blank(),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_line(color = "gray88"))
+
+
+print(samplesbyyear)
+
+# Save the plot as an SVG
+ggsave(file = "samplesbyyear.svg", plot = samplesbyyear, 
+       width = 7, height = 2, dpi = 72) 
+  
